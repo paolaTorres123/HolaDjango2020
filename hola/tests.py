23e95fa04da66bd5092dbbc25fbe4aeaf9cd6ea8
paolaTorres123 <pao.paotorres18@gmail.com>
2020-02-12 11:53:21 -0600

    Se agregaron pruenas de modelos y formularios para el modulo de Articulos

--- hola/tests.py
+++ hola/tests.py
@@ -1,8 +1,63 @@
 from django.test import TestCase
+from hola.models import Articulo
+from django.core.exceptions import ValidationError
+from hola.forms import FormArticulo
 
 # Create your tests here.
 class TestSmoke(TestCase):
     def test_smoke_Test(self):
         self.assertEqual(2+2,4)
-    def test_smoke_Test(self):
-        self.assertEqual(2+3,5)
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.articulo = Articulo(
+            nombre ='Juguete',
+            precio=120.55,
+            descripcion='Juguete bonito'
+        )
+        self.articulo.save()
+    def test_agrega_articulo(self):
+        self.articulo.save()
+        self.assertEqual(Articulo.objects.count(),1)
+    def test_agrega_nombre_act_vacio(self):
+        self.articulo.save()
+        self.articulo.nombre=''
+        with self.assertRaises(ValidationError):
+            self.articulo.full_clean()
+class TestForms(TestCase):
+    def setUp(self,nombre="Juguete",precio=120.55,descripcion="Juguete bonito"):
+        
+        self.data={
+            'nombre':nombre,
+            'precio':precio,
+            'descripcion':descripcion
+
+        }
+    def test_formarticulo_valido(self):
+        form = FormArticulo(
+            self.data
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_nombre_act_vacio(self):
+        self.data['nombre']=''
+        form=FormArticulo(
+            self.data
+        )
+        self.assertFalse(form.is_valid())
+
+class TestViews(TestCase):
+    def setUp(self,nombre="Juguete",precio=120.55,descripcion="Juguete bonito"):
+        self.data={
+            'nombre':nombre,
+            'precio':precio,
+            'descripcion':descripcion
+
+        }
+    '''def test_url_articulos_alta(self):
+        response = self.client.get('/articulo/articulos')
+        self.assertEqual(response.status_code,200)'''
+
+    
+
+
